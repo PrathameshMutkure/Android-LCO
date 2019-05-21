8557db0ad84e1ce4425e34897b8be6898b3f830d
Prathamesh Mutkure <pmutkure009@gmail.com>
2019-05-21 20:17:41 +0530

    Finished Bulb App

--- Bulb-App/app/src/main/java/learn/lco/bulbapp/MainActivity.java
+++ Bulb-App/app/src/main/java/learn/lco/bulbapp/MainActivity.java
@@ -2,8 +2,28 @@ package learn.lco.bulbapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void blueTapped(View view) {
+
+        ImageView blue = findViewById(R.id.blueBulb);
+        ImageView green = findViewById(R.id.greenBulb);
+
+        blue.animate().alpha(1).setDuration(750);
+        green.animate().alpha(0).setDuration(750);
+    }
+
+    public void greenTapped(View view){
+
+        ImageView blue = findViewById(R.id.blueBulb);
+        ImageView green = findViewById(R.id.greenBulb);
+
+        blue.animate().alpha(0).setDuration(750);
+        green.animate().alpha(1).setDuration(750);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
